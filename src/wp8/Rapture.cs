7bef293edc0995804b0ce1584bf6aa9be22daccb
Jesse MacFadyen <purplecabbage@gmail.com>
2014-10-23 07:49:13 -0700

    added required capability for media library

--- src/wp8/Rapture.cs
+++ src/wp8/Rapture.cs
@@ -1,20 +1,8 @@
 
-
 using System;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
-using Microsoft.Phone.Info;
-using System.IO.IsolatedStorage;
-using System.Windows.Resources;
 using System.IO;
-using System.Diagnostics;
+using System.Windows;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using Microsoft.Xna.Framework.Media;
 
@@ -24,17 +12,19 @@ namespace WPCordovaClassLib.Cordova.Commands
     {
         public void captureScreen(string strArgs)
         {
-            var fileName = String.Format("WmDev_{0:}.jpg", DateTime.Now.Ticks);
+            var fileName = String.Format("AppScreenShot{0:}.jpg", DateTime.Now.Ticks);
 
-            WriteableBitmap bmpCurrentScreenImage = new WriteableBitmap((int)Application.Current.RootVisual.RenderSize.Width,
-                                                                        (int)Application.Current.RootVisual.RenderSize.Height);
-            bmpCurrentScreenImage.Render(Application.Current.RootVisual, new MatrixTransform());
-            bmpCurrentScreenImage.Invalidate();
-            SaveToMediaLibrary(bmpCurrentScreenImage, fileName, 100);
-            
-            //MessageBox.Show("Captured image " + fileName + " Saved Sucessfully", "WmDev Capture Screen", MessageBoxButton.OK);
+            Deployment.Current.Dispatcher.BeginInvoke(delegate
+            {
 
-            //currentFileName = fileName;
+                WriteableBitmap bmpCurrentScreenImage = new WriteableBitmap((int)Application.Current.RootVisual.RenderSize.Width,
+                                                                            (int)Application.Current.RootVisual.RenderSize.Height);
+                bmpCurrentScreenImage.Render(Application.Current.RootVisual, new MatrixTransform());
+                bmpCurrentScreenImage.Invalidate();
+                SaveToMediaLibrary(bmpCurrentScreenImage, fileName, 100);
+
+                DispatchCommandResult(new PluginResult(PluginResult.Status.OK, "All is good!"));
+            });
         }
 
         private void SaveToMediaLibrary(WriteableBitmap bitmap, string name, int quality)
